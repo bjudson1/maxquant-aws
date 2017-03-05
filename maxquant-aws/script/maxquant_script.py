6a1ece564127229da9b7da5d30a309431dd42584
Brenden Judson <Brenden@brendens-mbp.dhcp.nd.edu>
2017-03-04 21:43:18 -0500

    service and script

--- maxquant-aws/script/maxquant_script.py
+++ maxquant-aws/script/maxquant_script.py
@@ -37,16 +37,16 @@ EXP_NAME = args.pop(0)
  
 #--------Interupt Box Sync----------------------------------------------
 #find all running processes
-process_data = subprocess.check_output("tasklist", shell=True)
+#process_data = subprocess.check_output("tasklist", shell=True)
 #get the pid of boxsync.exe processes
-pid = re.findall('BoxSync\.exe\s+([0-9]{3}|[0-9]{4}) RDP',process_data,flags=0)
+#pid = re.findall('BoxSync\.exe\s+([0-9]{3}|[0-9]{4}) RDP',process_data,flags=0)
 
 #if len(pid) == 0:
  #   print "Error: BoxSync.exe not found"
 
 #Interupt all BoxSync.exe
-for current_pid in pid:
-    os.kill(int(current_pid), signal.SIGINT)
+#for current_pid in pid:
+  #  os.kill(int(current_pid), signal.SIGINT)
 
 #---------Get Info from xml-------------------------------------------
     
@@ -55,7 +55,9 @@ if os.path.isfile("{}".format(xml)):
     r = open(xml,'r')
     data = r.read()
     raw_files = re.findall('>(.+raw)',data,flags=0)
+    fasta = re.findall('>(.+fasta)',data,flags=0)
     threads = re.findall('numThreads>([0-9]{1}|[0-9]{2})',data,flags=0)
+
 else:
     print "Error: {} does not exist".format(xml)
     sys.exit(2)
@@ -66,29 +68,42 @@ os.mkdir("C:\Users\Administrator\Box Sync\{}".format(EXP_NAME))
 #close file
 r.close()
 
-#---------Create Info.txt-----------------------------------------------
-info_file = open("info.txt","w+")
+#---------Create file_paths.txt-----------------------------------------------
+file_paths = open("file_paths.txt","w+")
+text = ""
+text += xml + '\n'
+text += fasta[0] + '\n'
+for current_file in raw_files:
+    text += current_file + '\n'
+
+file_paths.write(text)
+file_paths.close()
 
 #---------Upload Files to Box Sync-------------------------------------------
+#move file_paths file
+print "Uploading file_paths.txt..."
+os.rename(".\\file_paths.txt", "C:\Users\Administrator\Box Sync\{0}\\file_paths.txt".format(EXP_NAME))  
+
 #move xml to box sync folder (upload)
 print "Uploading {}...".format(xml)
-info_file.write("{}\n".format(os.path.basename(xml)))
 os.rename("{}".format(xml), "C:\Users\Administrator\Box Sync\{0}\{1}".format(EXP_NAME,os.path.basename(xml)))  
+
+#move xml to box sync folder (upload)
+print "Uploading {}...".format(fasta[0])
+os.rename("{}".format(fasta[0]), "C:\Users\Administrator\Box Sync\{0}\{1}".format(EXP_NAME,os.path.basename(fasta[0])))  
 
 #loop through all found raw files
 for current_file in raw_files:
     #if the raw file exists, move to box sync folder (upload)
     if os.path.isfile("{}".format(current_file)):
         print "Uploading {}...".format(current_file)
-        #write raw file to info.txt
-        info_file.write("{}\n".format(os.path.basename(current_file)))
         os.rename("{}".format(current_file), "C:\Users\Administrator\Box Sync\{0}\{1}".format(EXP_NAME,os.path.basename(current_file))) 
     #else file doesnt exist reprot error and exit code
     else:
         print "Error: {} does not exist".format(current_file)
         sys.exit(2)
 #-------------Run Box Sync--------------------------------------------------
-os.system("C:\\Program File\\Box\\BoxSync\\BoxSync.exe")
+#os.system("C:\\Program File\\Box\\BoxSync\\BoxSync.exe")
 #os.system("C:\Program File\Box\BoxSync\BoxSync.exe")
 
 #-------------Wait for Sync--------------------------------------------------
@@ -102,18 +117,18 @@ os.system("C:\\Program File\\Box\\BoxSync\\BoxSync.exe")
 # threads <= vCPUs
 # max = 16 vCPU
 if int(threads[0]) <= 2:
-	my_instance_type = 'c3.large'
+	my_instance_type = 'c4.large'
 elif int(threads[0]) <= 4:
-	my_instance_type = 'c3.xlarge'
+	my_instance_type = 'c4.xlarge'
 elif int(threads[0]) <= 8:
-	my_instance_type = 'c3.2xlarge'
+	my_instance_type = 'c4.2xlarge'
 else:
-	my_instance_type = 'c3.4xlarge'
+	my_instance_type = 'c4.4xlarge'
 
 #Connect to region
 try:
     ## Get connection to aws EC2.	
-    conn = boto.ec2.connect_to_region("us-west-2",aws_access_key_id='<aws_access_key_id>',aws_secret_access_key='<aws_secret_access_key>')
+    conn = boto.ec2.connect_to_region("us-west-2",aws_access_key_id='aws_access_key_id',aws_secret_access_key='aws_secret_access_key')
 
 #Catch failed connection
 except Exception, ec:
@@ -123,11 +138,14 @@ except Exception, ec:
 
 #Create instance
 try:
-    conn.run_instances(
-    'ami-a3d253c3',
-    key_name = '<key_name>',
+    reservation = conn.run_instances(
+    'ami-9dfb76fd',
+    key_name = 'brendenAWSchem',
     instance_type = my_instance_type,
     security_groups = ['launch-wizard-4'])
+
+    for instance in reservation.instances:
+    	instance.add_tag("Experiment-Name", EXP_NAME)
 
 #Catch failed creation
 except Exception, ec:
--- maxquant-aws/service/maxquant_service.py
+++ maxquant-aws/service/maxquant_service.py
@@ -9,10 +9,17 @@ import shutil
 import subprocess
 import signal
 import fileinput
+import time
+import boto.ec2
 
 #----------Global variables----------------------
 TEMP_PATH = ''
 EXP_NAME = ''
+FROM = "MaxQuant"
+EMAIL_TO_SEND_TO = "brenden.judson@gmail.com"
+SUBJECT = "hi"
+TEXT = email_message
+EMAIL_TEXT = "Your MaxQuant test {} has failed to complete."
 
 #------------Functions--------------------------
 def usage(status=0):
@@ -25,57 +32,90 @@ def usage(status=0):
 
 #question if needed since shut down vm
 def cleanup():
+    #-------Send Email-------------------------------------------------
+
+    message = """\
+    From: MaxQuant
+    To: %s
+    Subject: MaxQuant Update
+
+    %s
+    """ % (", ".join(EMAIL_TO_SEND_TO), TEXT)
+
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.ehlo()
+    server.starttls()
+    server.login("brenden.judson@gmail.com", getpass.getpass())
+    server.sendmail("brenden.judson@gmail.com", EMAIL_TO_SEND_TO, message)
+    server.quit()
+
     shutil.rmtree(TEMP_PATH)
+
+    #instance = conn.get_only_instances(instance_ids=[instance_id])[0]
+	# if instance.state == "running":
+	#	print "Stopping instance %s" %  str(instance_id)
+	#	instance.stop()
 
  #----------Setup------------------------------
 #Create temp dir
 TEMP_PATH =  tempfile.mkdtemp()
 
 #Register cleanup
-atexit.register(cleanup)
+#atexit.register(cleanup)
 
 #Create list of passed arguments
-args = sys.argv[1:]
+#args = sys.argv[1:]
 
 #trap if incorrect number of arguements passed
-if len(args) != 1:
-    print "Error01: Incorrect number of parameters."
-    usage(1)
+#if len(args) != 1:
+ #   print "Error01: Incorrect number of parameters."
+ #   usage(1)
 
 #get experiment name
-EXP_NAME = args.pop(0)
+#EXP_NAME = args.pop(0)
+
+try:
+    ## Get connection to aws EC2.	
+    conn = boto.ec2.connect_to_region("us-west-2",aws_access_key_id='aws_access_key_id',aws_secret_access_key='aws_secret_access_key')
+
+#Catch failed connection
+except Exception, ec:
+    error = " %s " % str(ec)
+    print error
+    sys.exit(5)
+
+command = 'powershell (Invoke-WebRequest -Uri http://169.254.169.254/latest/meta-data/instance-id).Content'
+instance_id = subprocess.check_output('"' + command + '"', shell=True).strip()
+
+tags = conn.get_only_instances(instance_ids=[instance_id])[0].tags["Experiment-Name"]
+#os.system('"' + command + '"')
+EXP_NAME = tags
 
 #--------Start Box Sync----------------------------------------------
 #os.system("C:\\Program\ File\\Box\\BoxSync\\BoxSync.exe")
 
-boxsync = 'C:\\Program Files\\Box\\Box Sync\\BoxSync.exe'
-subprocess.call([boxsync])
+#boxsync = 'C:\\Program Files\\Box\\Box Sync\\BoxSync.exe'
+#subprocess.call([boxsync])
 
 #--------Wait for Box Sync----------------------------------------------
+while not os.path.isfile("C:\Users\Administrator\Box Sync\{0}\\file_paths.txt".format(EXP_NAME)):
+    time.sleep(5)
 
 #--------Interupt Box Sync----------------------------------------------
 #find all running processes
-process_data = subprocess.check_output("tasklist", shell=True)
+#process_data = subprocess.check_output("tasklist", shell=True)
 #get the pid of boxsync.exe processes
-pid = re.findall('BoxSync\.exe\s+([0-9]{3}|[0-9]{4}) RDP',process_data,flags=0)
+#pid = re.findall('BoxSync\.exe\s+([0-9]{3}|[0-9]{4}) RDP',process_data,flags=0)
 
-if len(pid) == 0:
-   print "Error: BoxSync.exe not found"
+#if len(pid) == 0:
+#   print "Error: BoxSync.exe not found"
 
 #Interupt all BoxSync.exe
-for current_pid in pid:
-    os.kill(int(current_pid), signal.SIGINT)
-
-#------------Move Exp Dir to Temp Dir---------------------
-if os.path.isdir("C:\Users\Administrator\Box Sync\{0}".format(EXP_NAME)):
-    print "Downloading {} directory...".format(EXP_NAME)
-    os.rename("C:\Users\Administrator\Box Sync\{0}".format(EXP_NAME),"{0}\{1}".format(TEMP_PATH,EXP_NAME))
-else:
-    print "Error02: experiment directory not found"
-    sys.exit(2)
+#for current_pid in pid:
+ #   os.kill(int(current_pid), signal.SIGINT)
 #-----------Read file_paths File-----------------------------------------
-if os.path.isfile("{}\{}\\file_paths.txt".format(TEMP_PATH,EXP_NAME)):
-    r = open("{}\{}\\file_paths.txt".format(TEMP_PATH,EXP_NAME),'r')
+if os.path.isfile("C:\Users\Administrator\Box Sync\{0}\\file_paths.txt".format(EXP_NAME)):
+    r = open("C:\Users\Administrator\Box Sync\{0}\\file_paths.txt".format(EXP_NAME),'r')
     file_paths_data = r.read()
 
     raw_file_paths = re.findall('.+\.raw',file_paths_data,flags=0)
@@ -99,46 +139,52 @@ else:
 #close file_paths file
 r.close()
 
+#--------Wait for Box Sync----------------------------------------------
+while not os.path.isfile("C:\Users\Administrator\Box Sync\{}\{}".format(EXP_NAME,os.path.basename(xml_path[0]))):
+    time.sleep(5)
+while not os.path.isfile("C:\Users\Administrator\Box Sync\{}\{}".format(EXP_NAME,os.path.basename(fasta_path[0]))):
+    time.sleep(5)
+for current_file in raw_file_paths:
+    while not os.path.isfile("C:\Users\Administrator\Box Sync\{}\{}".format(EXP_NAME,os.path.basename(current_file))):
+        time.sleep(5)
+
+#------------Move Exp Dir to Temp Dir---------------------
+if os.path.isdir("C:\Users\Administrator\Box Sync\{0}".format(EXP_NAME)):
+    print "Downloading {} directory...".format(EXP_NAME)
+    os.rename("C:\Users\Administrator\Box Sync\{0}".format(EXP_NAME),"{0}\{1}".format(TEMP_PATH,EXP_NAME))
+else:
+    print "Error02: experiment directory not found"
+    sys.exit(2)
+
 #-----------Edit XML----------------------
 #open file w/ read write
 xml_file = open("{}\{}\{}".format(TEMP_PATH,EXP_NAME,os.path.basename(xml_path[0])),'r+')
 
+text = ""
 
-#for line in fileinput.input( "{}\{}\{}".format( TEMP_PATH,EXP_NAME,os.path.basename(xml_path[0]) ) ):
-for line in xml_file.read():
-	#update path of raw files
+for line in xml_file:
+   
     for current_file_path in raw_file_paths:
-        if "{}".format(current_file_path) in line :
-            print "Updated {} in XML".format(os.path.basename(current_file_path))
-        else:
-            print "Failed to update in {} XML".format(os.path.basename(current_file_path))
-        xml_file.write( line.replace("{}".format(current_file_path),"{}\{}".format(TEMP_PATH,os.path.basename(current_file_path))))
+         line = line.replace("{}".format(current_file_path),"{}\{}\{}".format(TEMP_PATH,EXP_NAME,os.path.basename(current_file_path)))
 
-    #update path of .fasta file
-    if "{}".format(fasta_path[0]) in line :
-        print "Updated {} in XML".format(os.path.basename(fasta_path[0]))
-    else:
-        print "Failed to update in {} XML".format(os.path.basename(fasta_path[0]))
-
-    xml_file.write( line.replace( "{}".format(fasta_path[0]), "{}\{}".format(TEMP_PATH,os.path.basename(fasta_path[0])) ) )
-
-#close file
+    line = line.replace("{}".format(fasta_path[0]),"{}\{}\{}".format(TEMP_PATH,EXP_NAME,os.path.basename(fasta_path[0])))
+    text += line
+    
+xml_file.seek(0)
+xml_file.write(text)
+xml_file.truncate()
 xml_file.close()
-#-----------Run MaxQuant------------------
-#os.system('C:\Users\Administrator\Desktop\MaxQuant_1.5.5.1\MaxQuant\MaxQuant.exe C:{0}\{1}'.format(TEMP_PATH,xml))
 
-#maxquant = 'C:\\Users\\Administrator\\Desktop\\MaxQuant_1.5.5.1\\MaxQuant\\MaxQuant.exe C:{}\{}'.format(TEMP_PATH,xml)
-#subprocess.call([maxquant])
+#-----------Run MaxQuant------------------
 # data trao for xml not in temp
-print os.listdir('{}\{}'.format(TEMP_PATH,EXP_NAME))
-if not os.path.isfile('{0}\{1}\{2}'.format(TEMP_PATH,EXP_NAME,xml[0])):
+if not os.path.isfile('{0}\{1}\{2}'.format(TEMP_PATH,EXP_NAME,os.path.basename(xml_path[0]))):
     print "Error03: xml not in temp dir"
     sys.exit(4)
 
 print "Begin MaxQuant..."
 
-file = 'C:\\Users\\Administrator\\Desktop\\MaxQuant_1.5.5.1\\MaxQuant\\bin\\MaxQuantCmd.exe {}\{}\{}'.format(TEMP_PATH,EXP_NAME,xml_path[0])
-os.system('"' + file + '"')
+maxquant = 'C:\\Users\\Administrator\\Desktop\\MaxQuant_1.5.5.1\\MaxQuant\\bin\\MaxQuantCmd.exe {}\{}\{}'.format(TEMP_PATH,EXP_NAME,os.path.basename(xml_path[0]))
+os.system('"' + maxquant + '"')
 
 #----------Wait till MaxQuant finishes----------------------------
 process_data = subprocess.check_output("tasklist", shell=True)
@@ -149,17 +195,18 @@ while len(running_maxs) != 0:
     process_data = subprocess.check_output("tasklist", shell=True)
     #get the pid of MaxQuant.exe processes
     running_maxs = re.findall('MaxQuant\.exe\s+([0-9]{3}|[0-9]{4}) RDP',process_data,flags=0)
-    syxs.
+    time.sleep(60)
 
 #-------Upload Combined Folder-------------------------------------
-#------Kill VM-----------------------------------------------------
+os.mkdir("C:\Users\Administrator\Box Sync\{}_output".format(EXP_NAME))
 
- #instance = conn.get_only_instances(instance_ids=[instance_id])[0]
-	# if instance.state == "running":
-	#	print "Stopping instance %s" %  str(instance_id)
-	#	instance.stop()
+if os.path.isdir('{0}\{1}\combined'.format(TEMP_PATH,EXP_NAME)):
+    os.rename('{0}\{1}\combined'.format(TEMP_PATH,EXP_NAME),"C:\Users\Administrator\Box Sync\{}_output\combined".format(EXP_NAME))
+else:
+    print "Error03: could not find combined folder"   
 
-
+EMAIL_TEXT = "Your MaxQuant test {} has successfuly completed."
+ 
 sys.exit(0)
 
 
